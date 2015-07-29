ee7d44555b8a0d57aa3da8e8c0ee08b29acad2c9
Haga Andriamihamina-Radaody <haga.radaody@gmail.com>
2015-07-29 13:52:19 +0200

    erase commits
    dfg

--- ArduinoSensorReading/AnalogInput_Pt100/AnalogInput_Pt100.ino
+++ ArduinoSensorReading/AnalogInput_Pt100/AnalogInput_Pt100.ino
@@ -1,12 +1,8 @@
 int sensorPin = A0;    // select the input pin for the potentiometer
-//int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
   SerialUSB.begin(9600);
-  //analogReference(INTERNAL);
-  //pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
