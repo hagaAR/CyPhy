afa57c12b0df6355668739fe7d3153e5f1e1a19b
Haga Andriamihamina-Radaody <haga.radaody@gmail.com>
2015-08-11 16:28:17 +0200

    Get Data
    Problems with reading/handling received data from RP

--- ArduinoSensorReading/ThetaRho/ThetaRho.ino
+++ ArduinoSensorReading/ThetaRho/ThetaRho.ino
@@ -1,4 +1,5 @@
 #include <XBee.h>
+#include<stdlib.h>
 
 int sensorPin = A0;    // select the input pin
 int sensorValue = 0;  // variable to store the value coming from the sensor
@@ -58,8 +59,8 @@ float readSensorPT100() {
 }
 
 //void write_data(char string[]){
-void write_data(){
-  char string[]="hfzsedfz RP, iiukuikm arduinoerztefzgefgyzegfiglgfbegrgergkzerguerugehrgkjzehriuhvuheha fu erfu e rhua u aeu ra ";
+ void write_data(){
+  char string[]="executeRPcommand";
   //Serial.println(sizeof(string));
   for(int i=0;i<sizeof(string);i++){
     payload_SensorData_String[i]=string[i];
@@ -75,44 +76,106 @@ void write_data(){
   }
 
   
-}
+} 
 
+void execute_RP_command(){
+    //char data[sizeof(receive_dataFromRP())] ;
+    char * abc;
+    char*  data =receive_dataFromRP(abc);
+     Serial.print("data read: ");
+    Serial.println(data);
+    //Serial.println(sizeof(receive_dataFromRP()));
+	  
+//	//Reads data frames from RP
+//	//frames structures should be like: 'getData;Sensor#1'
+//    int commandStringSize=0;
+//    int sensorStringSize=0;
+//    for(int i=0;i<sizeof(data);i++){
+//  		if(&data[i]==";"){
+//  			commandStringSize = i+1;
+//        Serial.println("enter");
+//  			break;
+//  		} 
+//    }
+//    for(int i=commandStringSize-1;i<sizeof(data);i++){
+//      if(&data[i]==";"){
+//        sensorStringSize = i+1;
+//        break;
+//      } 
+//    }
+//	char command[commandStringSize];
+//	char sensor[sensorStringSize];
+//	for(int k=0;k<commandStringSize-1;k++){
+//		command[k]=data[k];
+//	} 
+//
+//	for(int k=commandStringSize-1;k<sensorStringSize-1;k++){
+//		sensor[k]=data[k];
+//	}	
+//		
+//	//Writes msg to RP
+//	if(command =="getData"){
+//		//should be a 'switch case'
+//		if(sensor=="Thermo1"){
+//			String msgToRP = "Thermo1;";
+//			for(int i=0;i<sizeof(msgToRP);i++){
+//				payload_SensorData[i]=(uint8_t)msgToRP[i];
+//			}
+//			payload_SensorData[sizeof(msgToRP)]=(uint8_t)readSensorPT100(); 
+//			send_SensorData_toRP(payload_SensorData);
+//		}
+//	} else {
+//		Serial.println(command);
+//	}
+//Serial.println(command);
+//Serial.println(sensor);
+ 
+}
 
 void send_SensorData_toRP(uint8_t payload_tab[]) {   
   xbee.send(zbTx);
 }
 
 
-void receive_dataFromRP(){
-	xbee.readPacket(); 
+char * receive_dataFromRP(char* c){
+	xbee.readPacket();
     if (xbee.getResponse().isAvailable()) {
       // got something
       if (xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+        xbee.getResponse().getZBRxResponse(rx);
         // got a zb rx packet	
-				Serial.println("here is the packet");
-				processPayload(rx.getData(),rx.getDataLength());
-//				for (int i=0;i<rx.getDataLength();i++){
-//					Serial.print((char)rx.getData(i));
-//          Serial.print(" ");
-//				}
-				Serial.println("");
-				Serial.println("ending reading");
+				//Serial.print("message from RP: ");
+				//processPayload(rx.getData(),rx.getDataLength());
+        c[rx.getDataLength()+1]; 
+        //Serial.print("Size of zigbee payload :");
+        //Serial.println(rx.getDataLength());
+				for (int i=0;i<rx.getDataLength();i++){
+					//Serial.print((char)rx.getData(i));
+          //Serial.print(" ");
+          c[i] =(char)rx.getData(i);
+				}
+				//Serial.println("");
+				Serial.print(" c: ");
+        Serial.println(c);
+        
+       return c;
       } 
     } else if (xbee.getResponse().isError()) {
 			Serial.print("Error reading packet.  Error code: ");
 			Serial.println(xbee.getResponse().getErrorCode());
     }
+ return 0;   
 }
 
 void loop (){
-  uint8_t Temperature=(uint8_t)readSensorPT100();
+  //uint8_t Temperature=(uint8_t)readSensorPT100();
   //char string[]="hfzsedfz RP, iiukuikm arduino";
   //write_data(write_data(string););
 
 	//write_data();
   //send_SensorData_toRP(payload_SensorData);
   
-  receive_dataFromRP();
-  
+  //receive_dataFromRP();
+  execute_RP_command();
   delay(1000);
 }
