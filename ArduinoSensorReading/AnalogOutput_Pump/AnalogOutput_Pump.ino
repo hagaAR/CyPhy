15d230a83cc2bddb4df8c8587fa8f1a07fe0c05e
Haga Andriamihamina-Radaody <haga.radaody@gmail.com>
2015-10-07 11:23:38 +0200

    Better

--- ArduinoSensorReading/AnalogOutput_Pump/AnalogOutput_Pump.ino
+++ ArduinoSensorReading/AnalogOutput_Pump/AnalogOutput_Pump.ino
@@ -1,27 +1,11 @@
-//int sensorPin=A0;
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-//  pinMode(2,OUTPUT);
-//  pinMode(3,OUTPUT);
 }
 
 void loop(){
-  // read the input on A0 and map it to a PWM pin
-  // with an attached LED
-
-    // the default PWM resolution
+   // the default PWM resolution
   analogWriteResolution(10);
   analogWrite(2,0);
   analogWrite(3,1023);
-  
-//  int sensorVal = analogRead(A0);
-//  Serial.print("Analog Read) : ");
-//  //Serial.print(HIGH);
-//   Serial.print("  ");
-//  Serial.println(sensorVal);
 
-  //delay(1007);
-//  analogWrite(2,3500);
-//  delay(1007);
 }
--- ArduinoSensorReading/XBeeTx/XBeeTx.ino
+++ ArduinoSensorReading/XBeeTx/XBeeTx.ino
@@ -1,10 +1,7 @@
 #include <XBee.h>
-
 // create the XBee object
 XBee xbee = XBee();
-
 uint8_t payload[] = { 23, 0 };
-
 // SH + SL Address of receiving XBee
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x40C0E60A);
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
@@ -18,21 +15,16 @@ void setup() {
 
 void loop() {   
   // break down 10-bit reading into two bytes and place in payload
-
   payload[0] = 0xff;
   payload[1] = 0xff;
-
   xbee.send(zbTx);
-
    // after sending a tx request, we expect a status response
   // wait up to half second for the status response
   if (xbee.readPacket(500)) {
     // got a response!
-
     // should be a znet tx status              
     if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
       xbee.getResponse().getZBTxStatusResponse(txStatus);
-
       // get the delivery status, the fifth byte
       if (txStatus.getDeliveryStatus() == SUCCESS) {
         // success.  time to celebrate
